a32ef0156771096c06446b2921e6ee97c92564ff
galuhadipr@gmail.com <galuhadipr@gmail.com>
2024-08-12 14:54:00 +0700

    develop task crud

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Redirect, Render } from '@nestjs/common';
+import { Task } from './dto/task-dto';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 
 @Controller('task')
 export class TaskController {
     @Get()
     @Render('task/index')
-    async index(): Promise<{ pageTitle: string }> {
+    async index(): Promise<{ pageTitle: string; tasks: Task[] }> {
+        const tasks: Task[] = await prisma.task.findMany();
       return {
         pageTitle: 'Tasks',
+        tasks,
       };
     }
 
@@ -22,26 +29,75 @@ export class TaskController {
     @Render('task/edit')
     async edit(
         @Param('id') id: number,
-    ): Promise<{ id: number; pageTitle: string }> {
+    ): Promise<{ pageTitle: string; task:Task; dueDate: string }> {
+        const task: Task = await prisma.task.findUnique({
+            where: {
+              id: Number(id),
+            },
+          });
+        const dueDate = new Date(task.dueDate).toISOString().split('T')[0];
+
     return {
-        id,
       pageTitle: 'Edit Task',
+      task,
+      dueDate,
     };
+     }
+
+     @Put(':id/update')
+     @Redirect('/task')
+     async update(@Param('id') id: number, @Body() task: Task) {
+       const data = {
+         ...task,
+         dueDate: new Date(task.dueDate),
+       };
+   
+       await prisma.task.update({
+         where: {
+           id: Number(id),
+         },
+         data,
+       });
      }
 
     @Get(':id/delete')
     @Render('task/delete')
     async delete(
         @Param('id') id: number,
-    ): Promise<{ id:number; pageTitle: string }> {
+    ): Promise<{ pageTitle: string; task: Task }> {
+        const task: Task = await prisma.task.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+
     return {
-        id,
-      pageTitle: 'Create Task',
+      pageTitle: 'Delete Task',
+      task,
     };
     }
 
-    @Get(':id/detail')
-    detail(@Param('id') id: number): string {
-      return `Mengembalikan data task detail dengan id: ${id}`;
+    @Delete(':id/destroy')
+    @Redirect('/task')
+    async destroy(@Param('id') id: string) {
+      await prisma.task.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+    }
+
+
+    @Post('store')
+    @Redirect('/task')
+    async store(@Body() task: Task) {
+      const data = {
+        ...task,
+        dueDate: new Date(task.dueDate),
+      };
+  
+      await prisma.task.create({
+        data,
+      });
     }
 }
